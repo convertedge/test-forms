332e08c9ef43a5efa3125615fc9ff3c98f9ad5aa
Nir Galon <nir@galons.io>
2026-01-18 14:12:54 +0200

    foo

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,25 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        // key = final output path, value = source html file
+        main: resolve(__dirname, "index.html"),
+        complex: resolve(__dirname, "complex.html"),
+        "email-text": resolve(__dirname, "email-text.html"),
+        "no-form": resolve(__dirname, "no-form.html"),
+        "one-email": resolve(__dirname, "one-email.html"),
+        "one-phone": resolve(__dirname, "one-phone.html"),
+        "phone-and-email": resolve(__dirname, "phone-and-email.html"),
+        "phone-phone": resolve(__dirname, "phone-phone.html"),
+        "phone-text": resolve(__dirname, "phone-text.html"),
+        "shortify-test": resolve(__dirname, "shortify-test.html"),
+        "two-email": resolve(__dirname, "two-email.html"),
+      },
+    },
+  },
 });
